c59090597b99d960d340595f1d9e4f3d250cf632
Shubham Sihasane <78297361+shubham-sihasane@users.noreply.github.com>
2021-11-11 22:36:52 +0530

    Add files via upload

---
+++ 1.HelloWorld.py
@@ -0,0 +1,19 @@
+
+  
+############################################################################
+
+# Author        : Shubham Sihasane
+# Date          : 11th November, 2021
+# Description   : Write a simple python program to print a message on the console screen.
+
+############################################################################
+
+
+# Print a text on sinle line
+print("Hello World!")
+
+# Print a text on multiple lines.
+print("Welcome to the World of Python\nThe next big thing is nothing but the DevOps!")
+
+
+##############################END OF THE SCRIPT##############################
